1daf34e1def7ff9dfeffc1a17cb5d67db2878671
lilyH <hlily871108@gmail.com>
2014-08-18 16:59:28 +0800

    first commit
    add the code

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ pageSwitch
 ==========
 
 switching pages in web app with transitions
+单页应用中页面之间的转场切换
+
+## 说明
+* （1）依赖于zepto.js
+* （2）代码中加入了zepto-touch.js 为了支持swipeRight与swipeLeft 动作，实现页面的切换
+
+## 功能
+* （1）支持swipeRight与swipeLeft切换到上一页与下一页
+* （2）支持制定目标页（对应的hash值），切换到对应的页面
+
+## 原理
+
+利用不同的hash值，对应不同的页面；通过对hash的控制达到显示不同的页面；页面之间的转场动画利用transition动画实现
---
+++ pageswitch.js
@@ -0,0 +1,83 @@
+function PageSwitcher(){
+    var currentPage,
+        stateHistory = [],
+        stateTree = [];
+    this.switchPage = function(){
+        var hisLen = stateHistory.length,
+            treeLen = stateTree.length,
+            state = window.location.hash,
+            j = -1,k=-1;
+
+        for(var i=0 ;i<hisLen ;i++){
+            if(state == stateHistory[i]){
+                j = i;
+                break;   
+            }
+        }
+        for(var i=0;i<treeLen;i++){
+            if(state == stateTree[i]){
+                k = i;
+                break;
+            }
+        }
+        
+        if(j !== -1){//the previous page
+            for(var i=j;i<hisLen-1;i++){
+                page = currentPage.prev('.page');
+                if(page && page.length > 0){
+                    currentPage.attr('class','page transition right100');
+                    currentPage = page;
+                    stateHistory.pop();
+                }
+            }
+        }else{//the next page
+            for(var i=0; i<k; i++){
+                page = currentPage.next('.page');
+                if(page && page.length > 0){
+                    stateHistory.push(stateTree[hisLen+i]);
+                    page.attr('class','page transition center');
+                    currentPage = page;
+                }
+            }
+            
+        }
+    },  
+    this.gotoPrevPage = function(){
+        page = currentPage.prev('.page');
+        if(page && page.length > 0){
+            currentPage.attr('class','page transition right100');
+            currentPage = page;
+            stateHistory.pop();
+        }
+    },
+    this.gotoNextPage = function(){
+        var hisLen = stateHistory.length;
+        page = currentPage.next('.page');
+        if(page && page.length > 0){
+            stateHistory.push(stateTree[hisLen+1]);
+            page.attr('class','page transition center');
+            currentPage = page;
+        }
+    },
+    this.init = function(obj){
+        
+        stateTree= obj.hashs;
+        var start = 0;
+        for(var i=0; i<stateTree.length ;i++){
+            if(stateTree[i] === obj.curHash){
+                start = 1;
+                currentPage = $($('.page')[i]).addClass('current');
+                page = currentPage;
+                stateHistory.push(stateTree[i]);
+            }
+            if(start == 1){
+                page = page.next('.page');
+                if(page && page.length > 0){
+                    page.attr('class','page transition right100');
+                }
+            }else{
+                stateHistory.push(stateTree[i]);
+            }
+        }
+    }
+}
